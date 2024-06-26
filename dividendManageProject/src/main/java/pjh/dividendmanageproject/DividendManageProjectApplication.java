6e56bfbf5be10a69cda8086a50cd0692b8903f36
joung-pjh <jounghyeon123@daum.net>
2024-06-26 18:51:17 +0900

    exercise how to use spring function to dividendManageProject

---
+++ dividendManageProject/src/main/java/pjh/dividendmanageproject/DividendManageProjectApplication.java
@@ -0,0 +1,13 @@
+package pjh.dividendmanageproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DividendManageProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DividendManageProjectApplication.class, args);
+    }
+
+}
---
+++ dividendManageProject/src/test/java/pjh/dividendmanageproject/DividendManageProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pjh.dividendmanageproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DividendManageProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
