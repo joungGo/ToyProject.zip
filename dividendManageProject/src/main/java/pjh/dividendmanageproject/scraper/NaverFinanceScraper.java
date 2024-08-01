1d67b3effd587a52ffb2dc559428ea6d7c086665
joung-pjh <jounghyeon123@daum.net>
2024-08-01 16:08:24 +0900

    restart

--- dividendManageProject/src/main/java/pjh/dividendmanageproject/scraper/NaverFinanceScraper.java
+++ dividendManageProject/src/main/java/pjh/dividendmanageproject/scraper/NaverFinanceScraper.java
@@ -1,4 +1,19 @@
 package pjh.dividendmanageproject.scraper;
 
-public class NaverFinanceScraper {
+import pjh.dividendmanageproject.model.Company;
+import pjh.dividendmanageproject.model.ScrapedResult;
+
+public class NaverFinanceScraper implements Scraper {
+
+    // TODO : YahooFinanceScrapper와 구현 방법 및 구성이 똑같기 때문에 생략! => 나중에 remind 용도로 해보기!!!!
+
+    @Override
+    public ScrapedResult scrap(Company company) {
+        return null;
+    }
+
+    @Override
+    public Company scrapCompanyByTicker(String ticker) {
+        return null;
+    }
 }
--- dividendManageProject/src/main/java/pjh/dividendmanageproject/scraper/YahooFinanceScrapper.java
+++ dividendManageProject/src/main/java/pjh/dividendmanageproject/scraper/YahooFinanceScrapper.java
@@ -14,7 +14,6 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.jar.JarOutputStream;
 
 public class YahooFinanceScrapper implements Scraper {
 
