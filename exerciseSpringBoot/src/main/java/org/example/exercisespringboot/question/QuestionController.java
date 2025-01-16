13ab14fcdf7d1b062d57eaf7cd4905b044e6129a
joung-pjh <jounghyeon123@daum.net>
2025-01-16 09:48:10 +0900

    ìfix constructor to controller-service-repository

--- exerciseSpringBoot/src/main/java/org/example/exercisespringboot/question/QuestionController.java
+++ exerciseSpringBoot/src/main/java/org/example/exercisespringboot/question/QuestionController.java
@@ -16,13 +16,18 @@ public QuestionController(QuestionRepository questionRepository) {
 @Controller
 public class QuestionController {
 
-    private final QuestionRepository questionRepository;
+    //private final QuestionRepository questionRepository;
+
+    // TODO : QuestionController 가 QuestionService 를 통해 QuestionRepository 를 사용하도록 수정
+    private final QuestionService questionService;
 
     @GetMapping("/question/list")
     //@ResponseBody // 템플릿 엔진을 사용하기 때문에 더이상 이 어노테이션은 필요X
     public String list(Model model) { // Model 객체는 자바 클래스(Java class)와 템플릿(template) 간의 연결 고리 역할, Model 객체에 값을 담아 두면 템플릿에서 그 값을 사용할 수 있다.
                                       // 키-값 쌍으로 데이터를 저장 :: key 는 템플릿에서 사용되는 변수 이름이고, "value"는 템플릿에서 렌더링될 데이터
-        List<Question> questionList = this.questionRepository.findAll();
+        //List<Question> questionList = this.questionRepository.findAll();
+        List<Question> questionList = this.questionService.getList();
+
         model.addAttribute("questionList", questionList);
         return "question_list";
     }
