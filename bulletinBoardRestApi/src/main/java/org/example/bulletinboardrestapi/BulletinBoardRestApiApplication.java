94debe695c7009b6a9735066611cb70c6a56cfc6
joung-pjh <jounghyeon123@daum.net>
2025-02-05 09:14:31 +0900

    feat:프로젝트 기초 환경 구축 1.DB: JPA/Docker-MySql 2.RestApi 개발 진행

---
+++ bulletinBoardRestApi/src/main/java/org/example/bulletinboardrestapi/BulletinBoardRestApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.bulletinboardrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulletinBoardRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BulletinBoardRestApiApplication.class, args);
+    }
+
+}
---
+++ bulletinBoardRestApi/src/test/java/org/example/bulletinboardrestapi/BulletinBoardRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bulletinboardrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulletinBoardRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
